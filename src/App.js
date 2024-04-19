3b9d0e3c13763dc319bd9c0cc37ec4536cd06ace
DevCrafts-SS <sakshams346@gmail.com>
2024-04-19 22:34:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1> hhmm</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.querySelector('body'));
+
+console.log(root)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
