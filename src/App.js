3460278583ae98734a1ddc26d9cbe941929310e8
DevCrafts-SS <sakshams346@gmail.com>
2024-04-28 11:58:03 +0530

    update scrollbar damping

--- src/App.js
+++ src/App.js
@@ -6,27 +6,21 @@ import Navigation from './Component/Navigation';
 import HomePage from './Component/HomePage';
 
 function App() {
-  const contentRef = useRef(null);
 
   useEffect(() => {
     Scrollbar.use(OverscrollPlugin);
 
     const scrollbar = Scrollbar.init(document.querySelector('#smooth'), {
-      damping: 0.05,
+      damping: 0.01,
       overscrollEffect: 'bounce',
       plugins: {
         overscroll: {
           effect: 'bounce',
-          damping: 0.1,
+          damping: 0.05,
           maxOverscroll: 100,
         },
       },
     });
-
-    // Adjust content height to allow scrolling beyond original position
-    const content = contentRef.current;
-    content.style.height = '200vh'; // Set the content height to 200% of the viewport height
-
     return () => {
       scrollbar.destroy();
     };
@@ -35,10 +29,8 @@ function App() {
   return (
     <>
       <div id='smooth' style={{ height: '100vh', overflow: 'auto' }}>
-        <div ref={contentRef}>
-          <Navigation />
-          <HomePage />
-        </div>
+        <Navigation />
+        <HomePage />
       </div>
     </>
   );
