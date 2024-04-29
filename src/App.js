60a6d28443f53a38f34cad275bc93c924b109cdc
DevCrafts-SS <sakshams346@gmail.com>
2024-04-29 21:52:54 +0530

    Update Images

--- src/App.js
+++ src/App.js
@@ -6,33 +6,33 @@ import Navigation from '../src/Component/Navigation';
 import HomePage from './Component/HomePage';
 
 function App() {
-  // useEffect(() => {
-  //   Scrollbar.use(OverscrollPlugin);
+  useEffect(() => {
+    Scrollbar.use(OverscrollPlugin);
 
-  //   const scrollbar = Scrollbar.init(document.querySelector('#smooth'), {
-  //     damping: 0.05,
-  //     overscrollEffect: 'bounce',
-  //     plugins: {
-  //       overscroll: {
-  //         effect: 'bounce',
-  //         damping: 0.05,
-  //         maxOverscroll: 100,
-  //       },
-  //     },
-  //   });
+    const scrollbar = Scrollbar.init(document.querySelector('#smooth'), {
+      damping: 0.05,
+      overscrollEffect: 'bounce',
+      plugins: {
+        overscroll: {
+          effect: 'bounce',
+          damping: 0.05,
+          maxOverscroll: 100,
+        },
+      },
+    });
 
-  //   // Clean up the smooth-scrollbar instance when the component unmounts
-  //   return () => {
-  //     scrollbar.destroy();
-  //   };
-  // }, []);
+    // Clean up the smooth-scrollbar instance when the component unmounts
+    return () => {
+      scrollbar.destroy();
+    };
+  }, []);
 
   return (
     <>
-      {/* <div id="smooth" style={{ height: '100vh', overflow: 'auto' }}> */}
+      <div id="smooth" style={{ height: '100vh', overflow: 'auto' }}>
         <Navigation />
         <HomePage />
-      {/* </div> */}
+      </div>
     </>
   );
 }
