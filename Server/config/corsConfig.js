dbbe2718c62a5f0e1f9be298bf457b6a22fdac04
Saksdev <sakshams346@gmail.com>
2026-03-01 11:24:06 +0530

    chore: Configure CORS to accept process.env.CLIENT_URL string for Render deployment

--- Server/config/corsConfig.js
+++ Server/config/corsConfig.js
@@ -1,7 +1,18 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    process.env.CLIENT_URL, // Allow the deployed frontend URL (e.g., Vercel)
+].filter(Boolean); // Remove undefined if CLIENT_URL is not set
+
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 };
 
